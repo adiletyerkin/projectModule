c2bc11a05f87410fa9f60acec2ae3e4ff44a4585
Adilet Yerkin <adilet.yerkin@gmail.com>
2021-04-01 23:17:38 +0600

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 
 @NgModule({
   declarations: [
     AppComponent
+=======
+import { AdminModuleComponent } from './admin-module/admin-module.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AdminModuleComponent
+>>>>>>> initial commit
   ],
   imports: [
     BrowserModule,
